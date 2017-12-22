f55c570ffa70105722b2e9957144ec27d186bf74
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2017-12-22 14:03:16 +0100

    version 0.2

--- check.js
+++ check.js
@@ -1,7 +1,8 @@
 (function(global) {
   var suites = {},
+    defer = setTimeout,
     checkObj = {
-      version: '0.1',
+      version: "0.2",
       suite: function(suiteName, onProgress) {
         suites[suiteName] = {
           tests: []
@@ -9,12 +10,13 @@
 
         function suite(name, fun) {
           checkObj.add(suiteName, name, fun);
+          return suite;
         }
         suite.run = function(onProgress) {
           checkObj.run(suiteName, onProgress);
         };
         if (onProgress) {
-          setTimeout(function() {
+          defer(function() {
             suite.run(onProgress);
           });
         }
@@ -25,10 +27,8 @@
         suites[suite].tests.push([desc, fun]);
       },
       run: function(suite, onProgress) {
+        suite = '' + suite;
         if (suites[suite]) {
-          //onprogress(completed, total, successes, failureReport, pendingReport, tests)
-          //test = [name, function]
-          //test[1](done) => done(TrueOrFailureString)
           var i,
             tests = suites[suite].tests,
             total = tests.length,
@@ -36,87 +36,75 @@
             passed = 0,
             failures = [],
             pending = [],
-            pendingReport;
+            pendingReport,
+            report;
+          if (typeof onProgress !== "function") {
+            report = document.getElementById("" + onProgress) ||
+              console || {};
+            onProgress = function(completed, total, successes,
+              failureReport, pending) {
+              var pendingLength = pending.length,
+                str = suite + "\n" + Array(suite.length + 1).join(
+                  '=') + '\n' + "Completed: " + completed +
+                " of " + total + "\nSuccesses: " + successes +
+                " of " + completed + (pendingLength ?
+                  "\nPending tests (" + pendingLength + "):\n" +
+                  (pendingLength > 3 ? pending.slice(0, 3).concat(
+                    '...') : pending).join("\n") : "") + (
+                  failureReport ? "\nFailing tests:\n" +
+                  failureReport.join("\n") : "");
+              if (report.log) {
+                report.log(str);
+              } else {
+                report.innerHTML = str;
+              }
+            };
+          }
 
           function update(index) {
-            completed++;
-            pending[index] = "";
-            pendingReport = pending
-              .join("\x1f")
-              .replace(/\x1f+/g, "\x1f")
-              .replace(/^\x1f/, "")
-              .replace(/\x1f$/, "");
-            onProgress(
-              completed,
-              total,
-              passed,
-              failures,
-              pendingReport ? pendingReport
-              .split("\x1f") : [],
-              tests
-            );
+            if (pending[index]) {
+              completed++;
+              pending[index] = "";
+            }
+            pendingReport = pending.join("\x1f").replace(/\x1f+/g,
+              "\x1f").replace(/^\x1f/, "").replace(/\x1f$/, "");
+            onProgress(completed, total, passed, failures,
+              pendingReport ? pendingReport.split("\x1f") : [],
+              tests);
           }
           for (i = 0; i < total; i++) {
-            //pending[i] = "" + i;
-            pending[i] = "[" + i + "] " + tests[i]
-              [0];
+            pending[i] = "[" + i + "] " + tests[i][0];
           }
+          update();
           for (i = 0; i < total; i++) {
             try {
               tests[i][1](
                 (function(index) {
-                  return function(
-                    result) {
-                    if (
-                      result === true ||
-                      result === void 0
-                    ) {
+                  return function(result) {
+                    if (result === true || result === void 0) {
                       passed++;
                     } else {
-                      failures
-                        .push(
-                          "[" +
-                          index +
-                          "] " +
-                          tests[
-                            index
-                          ]
-                          [
-                            0
-                          ] +
-                          ": " +
-                          (
-                            result ||
-                            "Failed"
-                          )
-                        );
+                      failures.push("[" + index + "] " +
+                        tests[index]
+                        [0] + ": " + (result || "Failed")
+                      );
                     }
-                    update(
-                      index
-                    );
+                    update(index);
                   };
-                })(i),
-                checkObj.eq
-              );
+                })(i), checkObj.eq);
             } catch (err) {
-              failures.push("[" + i + "] " +
-                tests[i][0] + ": " + err.message
-              );
+              failures.push("[" + i + "] " + tests[i][0] + ": " +
+                err.message);
               update(i);
             }
           }
         }
       },
       eq: function(a, b, priorA, priorB) {
-        var i = ""; // temp variable
-        // false if different types => falsy, 0 === [0], '' === []
+        var i = ""; // temp variable, default to empty string
         if (typeof a !== typeof b) return false;
-        // primitive values are compared as strings
-        if (typeof a !== "object") return a + i ===
-          b + i;
-        // false if string representations of objects differ, [] === {}, {} === null
+        if (typeof a !== "object") return a + i === b + i;
         if (a + i !== b + i) return false;
-        //check prior objects for circularity
         if (!priorA) {
           priorA = [];
           priorB = [];
@@ -130,17 +118,12 @@
         priorB.push(b);
         for (i in a) {
           // check if a and b has same properties with same values, recursively
-          if (
-            a.hasOwnProperty(i) &&
-            (!b.hasOwnProperty(i) || !checkObj.eq(
-              a[i], b[i], priorA, priorB))
-          )
-            return false;
+          if (a.hasOwnProperty(i) && (!b.hasOwnProperty(i) || !
+              checkObj.eq(a[i], b[i], priorA, priorB))) return false;
         }
         for (i in b) {
           // check if b has additional properties
-          if (b.hasOwnProperty(i) && !a.hasOwnProperty(
-              i)) return false;
+          if (b.hasOwnProperty(i) && !a.hasOwnProperty(i)) return false;
         }
         return true;
       }
